af1f4e39bff3f83728dff1f02f5b889201d28c51
Asadbek <asasedmor@gmail.com>
2025-08-10 18:17:57 +0900

    collected all packages

---
+++ client/app/layout.tsx
@@ -0,0 +1,33 @@
+import { ChildProps } from '@/types';
+import './globals.css';
+
+import type { Metadata } from 'next';
+import { Montserrat } from 'next/font/google';
+import React, { FC } from 'react';
+import { Toaster } from '@/components/ui/sonner';
+import Navbar from '@/components/shared/navbar';
+
+const montserrat = Montserrat({
+	weight: ['400', '500', '600', '700', '800', '900'],
+	subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+	title: 'Seleor e-commerce',
+	description: 'Seleor e-commerce website built with Next.js',
+	icons: { icon: '/favicon.png' },
+};
+
+const RootLayout: FC<ChildProps> = ({ children }) => {
+	return (
+		<html lang='en'>
+			<body className={`${montserrat.className} antialiased`}>
+				<Navbar />
+				<main className='container max-w-6xl mt-24'>{children}</main>
+				<Toaster />
+			</body>
+		</html>
+	);
+};
+
+export default RootLayout;
