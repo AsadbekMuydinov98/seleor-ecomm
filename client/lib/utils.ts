f3aa23a5be5b4caff79407acaf6f20eed0867fc2
Asadbek <asasedmor@gmail.com>
2025-08-11 16:39:30 +0900

    homepage created with testing products

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -1,6 +1,33 @@
+import { QueryProps } from "@/types";
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import qs from 'query-string'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+
+export function formatPrice(price: number) {
+	return new Intl.NumberFormat('kr-KR', { style: 'currency', currency: 'KRW' }).format(price);
+}
+
+export function formUrlQuery({params, key, value}: QueryProps) {
+  const currentUrl = qs.parse(params)
+  currentUrl[key] = value!
+
+  return qs.stringifyUrl({
+    url: window.location.pathname,
+    query: currentUrl
+  }, { skipNull: true })
+}
+
+export function removeUrlQuery({params, key}: QueryProps) {
+  const currentUrl = qs.parse(params)
+  delete currentUrl[key]
+
+  return qs.stringifyUrl({
+    url: window.location.pathname,
+    query: currentUrl
+  }, { skipNull: true })
 }
