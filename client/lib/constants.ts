96ef41fc6368f4ea002b81d595f7ff3a0e607fd8
Asadbek <asasedmor@gmail.com>
2025-08-11 17:26:45 +0900

    dashboard ui

--- client/lib/constants.ts
+++ client/lib/constants.ts
@@ -1,3 +1,5 @@
+import { Banknote, Heart, Settings2, Shuffle, User } from "lucide-react";
+
 export const products = [
 	{
 		_id: '1',
@@ -27,3 +29,11 @@ export const products = [
 
 
 export const categories = ['All', 'Shoes', 'T-Shirts', 'Clothes', 'Books', 'Accessories', 'Universal'];
+
+export const dashboardSidebar = [
+	{ name: 'Personal Information', route: '/dashboard', icon: User },
+	{ name: 'Orders', route: '/dashboard/orders', icon: Shuffle },
+	{ name: 'Payments', route: '/dashboard/payments', icon: Banknote },
+	{ name: 'Watch list', route: '/dashboard/watch-list', icon: Heart },
+	{ name: 'Settings', route: '/dashboard/settings', icon: Settings2 },
+];
